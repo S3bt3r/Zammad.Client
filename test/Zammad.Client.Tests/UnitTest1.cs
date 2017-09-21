a92e361164334b6faa3669c34a8e14e2fb7aa39d
Sebastian Richter <sebastian-richter@outlook.com>
2017-09-21 22:10:37 +0200

    Remove Test1Async

--- test/Zammad.Client.Tests/UnitTest1.cs
+++ test/Zammad.Client.Tests/UnitTest1.cs
@@ -9,25 +9,7 @@ namespace Zammad.Client.Tests
         [Fact]
         public async Task Test1Async()
         {
-            var account = ZammadAccount.CreateTokenAccount("http", "zammad-soft", "vatTlijkbmZYcIYQ1qhw8t6FIENwclwXYKQKPah34GKlolMb5e7jzZdxYjHY-IMP");
 
-            var groupClient = account.CreateGroupClient();
-            var groupList = await groupClient.GetGroupListAsync();
-
-            var notificationClient = account.CreateOnlineNotificationClient();
-            var notificationList = await notificationClient.GetOnlineNotificationListAsync();
-
-            var organizationClient = account.CreateOrganizationClient();
-            var organizationList = await organizationClient.GetOrganizationListAsync();
-
-            var ticketClient = account.CreateTicketClient();
-            var ticketList = await ticketClient.GetTicketListAsync();
-            var ticketArticleList = await ticketClient.GetTicketArticleListAsync();
-            var ticketPriorityList = await ticketClient.GetTicketPriorityListAsync();
-            var ticketStateList = await ticketClient.GetTicketStateListAsync();
-
-            var userClient = account.CreateUserClient();
-            var userList = await userClient.GetUserListAsync();
         }
     }
 }
