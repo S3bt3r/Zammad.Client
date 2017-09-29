8388291e8e6aa946dcfb54ee7a2e147591acce9d
Asesjix <asesjix@live.de>
2017-09-29 12:21:05 +0200

    Add test for Basic/TokenHttpClientHandler

--- test/Zammad.Client.Tests/Core/Protocol/BasicHttpClientHandlerTest.cs
+++ test/Zammad.Client.Tests/Core/Protocol/BasicHttpClientHandlerTest.cs
@@ -1,11 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
 
 namespace Zammad.Client.Core.Protocol
 {
     public class BasicHttpClientHandlerTest
     {
-        // ToDo: Add tests to BasicHttpClientHandlerTest
+        [Theory]
+        [InlineData("user", "password", "dXNlcjpwYXNzd29yZA==")]
+        [InlineData("zammad", "secure", "emFtbWFkOnNlY3VyZQ==")]
+        public async Task BasicHttpClientHandler_Success_Test(string user, string password, string expected)
+        {
+            using (var httpHandler = new BasicHttpClientHandler(user, password))
+            using (var httpClient = new HttpClient(httpHandler))
+            {
+                var response = await httpClient.GetAsync("http://zammad.com");
+                Assert.Equal("Basic", response.RequestMessage.Headers.Authorization.Scheme);
+                Assert.Equal(expected, response.RequestMessage.Headers.Authorization.Parameter);
+            }
+        }
+
+        [Theory]
+        [InlineData("zammad", "")]
+        [InlineData("", "secure")]
+        [InlineData("", "")]
+        [InlineData("zammad", null)]
+        [InlineData(null, "secure")]
+        [InlineData(null, null)]
+        public void BasicHttpClientHandler_Fail_Test(string user, string password)
+        {
+            Assert.ThrowsAny<ArgumentException>(() =>
+            {
+                var httpHandler = new BasicHttpClientHandler(user, password);
+            });
+        }
     }
 }
--- test/Zammad.Client.Tests/Core/Protocol/TokenHttpClientHandlerTest.cs
+++ test/Zammad.Client.Tests/Core/Protocol/TokenHttpClientHandlerTest.cs
@@ -1,11 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
 
 namespace Zammad.Client.Core.Protocol
 {
     public class TokenHttpClientHandlerTest
     {
-        // ToDo: Add tests to TokenHttpClientHandlerTest
+        [Theory]
+        [InlineData("token1", "token=token1")]
+        [InlineData("token2", "token=token2")]
+        public async Task TokenHttpClientHandler_Success_Test(string token, string expected)
+        {
+            using (var httpHandler = new TokenHttpClientHandler(token))
+            using (var httpClient = new HttpClient(httpHandler))
+            {
+                var response = await httpClient.GetAsync("http://zammad.com");
+                Assert.Equal("Token", response.RequestMessage.Headers.Authorization.Scheme);
+                Assert.Equal(expected, response.RequestMessage.Headers.Authorization.Parameter);
+            }
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        public void TokenHttpClientHandler_Fail_Test(string token)
+        {
+            Assert.ThrowsAny<ArgumentException>(() =>
+            {
+                var httpHandler = new TokenHttpClientHandler(token);
+            });
+        }
     }
 }
