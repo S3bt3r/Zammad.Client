6c8dcdef16ca3fdc649f84b3fe7c5802d4a95b1e
Robert Peerenboom <github@cloudvibe.de>
2023-09-19 20:45:38 +0200

    Erweiterung Links

---
+++ src/Zammad.Client/Resources/Assets.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using Newtonsoft.Json;
+
+namespace Zammad.Client.Resources
+{
+    [JsonObject]
+    public class Assets
+    {
+        [JsonProperty("Ticket")]
+        public Dictionary<string, Ticket> Ticket { get; set; }
+        
+        [JsonProperty("Group")]
+        public Dictionary<string, Group> Group { get; set; }
+        
+        [JsonProperty("User")]
+        public Dictionary<string, User> User { get; set; }
+
+        [JsonProperty("Role")]
+        public Dictionary<string, Role> Role { get; set; }
+
+        [JsonProperty("Organization")]
+        public Dictionary<string, Organization> Organization { get; set; }
+    }
+}
